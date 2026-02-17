6219fccfbf86f6f439ba4e0c73080ef091d148af
Pavel Novichkov <novickovpavel3@gmail.com>
2026-02-17 14:07:12 +0700

    add query parameters support

---
+++ src/main/java/ru/netology/Request.java
@@ -0,0 +1,63 @@
+package ru.netology;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Request {
+    private final String method;
+    private final String path;
+    private Map<String, List<String>> queryParams;
+    private final Map<String, String> headers;
+    private final String body;
+
+    public Request(String method, String path, String query, Map<String, String> headers, String body) {
+        this.method = method;
+        this.path = path;
+        try {
+            if (query != null && !query.isEmpty()) {
+                this.queryParams = URLEncodedUtils.parse(query, StandardCharsets.UTF_8).stream()
+                        .collect(Collectors.groupingBy(NameValuePair::getName,
+                                Collectors.mapping(NameValuePair::getValue, Collectors.toList())
+                        ));
+            } else {
+                this.queryParams = new HashMap<>();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.queryParams = new HashMap<>();
+        }
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Map<String, List<String>> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getQueryParam(String name) {
+        List<String> queryParam = queryParams.get(name);
+        return (queryParam != null && !queryParam.isEmpty()) ? queryParam.get(0) : null;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
