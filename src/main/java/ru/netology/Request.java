5b7958a3c780da9cd04230343628452fb3cc0d00
Pavel Novichkov <novickovpavel3@gmail.com>
2026-02-19 14:35:49 +0700

    add post params

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -3,7 +3,9 @@ package ru.netology;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +17,12 @@ public class Request {
     private Map<String, List<String>> queryParams;
     private final Map<String, String> headers;
     private final String body;
+    private Map<String, List<String>> postParams;
 
     public Request(String method, String path, String query, Map<String, String> headers, String body) {
         this.method = method;
         this.path = path;
+
         try {
             if (query != null && !query.isEmpty()) {
                 this.queryParams = URLEncodedUtils.parse(query, StandardCharsets.UTF_8).stream()
@@ -32,8 +36,22 @@ public class Request {
             e.printStackTrace();
             this.queryParams = new HashMap<>();
         }
+
         this.headers = headers;
         this.body = body;
+
+        this.postParams = new HashMap<>();
+        if (body != null && headers.get("Content-Type").equals("application/x-www-form-urlencoded")) {
+            String[] params = body.split("&");
+
+            for (String param : params) {
+                String[] parts = param.split("=",2);
+                String name = URLDecoder.decode(parts[0], StandardCharsets.UTF_8);
+                String value = URLDecoder.decode(parts[1], StandardCharsets.UTF_8);
+
+                postParams.computeIfAbsent(name, v -> new ArrayList<>()).add(value);
+            }
+        }
     }
 
     public String getMethod() {
@@ -59,5 +77,13 @@ public class Request {
 
     public String getBody() {
         return body;
+    }
+
+    public Map<String, List<String>> getPostParams() {
+        return postParams;
+    }
+
+    public List<String> getPostParam(String name) {
+        return postParams.get(name);
     }
 }
